0c7049f619790c252dba7e0f3c9a165162eaf943
Ostap Strashevskii <Ostap_Strashevskii@epam.com>
2020-04-21 21:54:24 +0300

    forth

--- src/main/scala/polishcalc/PolishCalculator.scala
+++ src/main/scala/polishcalc/PolishCalculator.scala
@@ -1,18 +1,5 @@
 package polishcalc
 
-
-
-//  sealed trait BinaryOperation{
-//    final val SumSign = '+'
-//    final val SubtractSign = '-'
-//    final val MultiplySign = '*'
-//    final val DevideSign = '/'
-//    final val LParenthesesSign = '('
-//    final val RParenthesesSign = ')'
-//  }
-
-
-
 object PolishCalculator {
 
   final val SumSign = '+'
@@ -21,26 +8,33 @@ object PolishCalculator {
   final val DevideSign = '/'
   final val LParenthesesSign = '('
   final val RParenthesesSign = ')'
+  final val MinPriority = 1
 
   def formatString(s: String) = {
     s.filterNot((x: Char) => x.isWhitespace)
   }
 
-  sealed trait Term //class trait, if i need constructor arguments
-  case class ElementaryTerm(leftOperand: Int, rightOperand: Int, operation: BinaryOperation) extends Term()
-  case class NonElementaryTerm(leftOperand: Term, rightOperand: Term, operation: BinaryOperation) extends Term()
+  sealed trait Term
+//  final case class ElementaryOperationTerm(leftOperand: NumberTerm, rightOperand: NumberTerm, operation: BinaryOperation) extends Term
+//  final case class NonElementaryTerm(terms: Seq[Term], operations: Seq[BinaryOperation]) extends Term
+//  final case class NumberTerm(number: Int) extends Term
 
-  sealed abstract class BinaryOperation(val sign: Char, priority: Int){
+  final case class OperationTerm(leftOperand: Term, rightOperand: Term, operation: BinaryOperation) extends Term
+  final case class NonElementaryTerm(terms: Seq[Term], operations: Seq[BinaryOperation]) extends Term
+  final case class NumberTerm(number: Int) extends Term
+
+
+  sealed abstract class BinaryOperation(val sign: Char, val priority: Int) {
     def operate(lOperand: Int, rOperand: Int): Int
   }
 
-  private final case class SumOperation() extends BinaryOperation(sign = SumSign, priority = 1){
+  final case class SumOperation() extends BinaryOperation(sign = SumSign, priority = 1) {
     override def operate(lOperand: Int, rOperand: Int): Int = {
       lOperand + rOperand
     }
   }
 
-  private final case class SubtractOperation() extends BinaryOperation(sign = SubtractSign, priority = 1){
+  final case class SubtractOperation() extends BinaryOperation(sign = SubtractSign, priority = 1) {
     override def operate(lOperand: Int, rOperand: Int): Int = {
       lOperand - rOperand
     }
@@ -58,88 +52,108 @@ object PolishCalculator {
     }
   }
 
-  def evaluateTerm(term: Term): Int = {//how to tailrec?? operate not evaluate
+  def evaluateTerm(term: Term): Int = {
     term match {
-      case ElementaryTerm(l,r,bop) => bop.operate(l,r)
-      case NonElementaryTerm(l,r,bop) => bop.operate(evaluateTerm(l), evaluateTerm(r))
+      case ElementaryOperationTerm(l,r,bop) => bop.operate(l.number, r.number)
+//      case NonElementaryTerm(l,r,bop) => bop.operate(evaluateTerm(l), evaluateTerm(r))
+      case NonElementaryTerm(terms,ops) =>
     }
   }
 
 
-  def getTermFromString() = {
 
+
+
+
+
+
+
+  @scala.annotation.tailrec
+  def getTermFromSeq(terms: Seq[Term], ops: Seq[BinaryOperation], priority: Int = MinPriority, acc: Seq[Term]): Seq[Term] = {
+
+    @scala.annotation.tailrec
+    def getTermsSeqForPriority(
+                                termsSeq: Seq[Term],
+                                opsSeq: Seq[BinaryOperation],
+                                previousTerm: Term,
+                                currentPriority: Int,
+                                termsAcc: Seq[Term] = Seq.empty[Term],
+                                opsAcc: Seq[BinaryOperation] = Seq.empty[BinaryOperation]): (Seq[Term], Seq[BinaryOperation]) = {
+      opsSeq match {
+        case op :: Nil => (termsAcc, opsAcc)
+        case op :: tail => if(op.priority == currentPriority) {
+
+
+
+          val newTerm = NonElementaryTerm(previousTerm, terms.head, op)
+          getTermsSeqForPriority(termsSeq.tail, tail, termsAcc.appended(NonElementaryTerm(previousTerm, terms.head, op)))
+
+
+        } else {
+          getTermsSeqForPriority(termsSeq.tail, tail, termsAcc.appended(terms.head), opsAcc.appended(ops.tail), currentPriority)
+        }
+      }
+    }
+    val termsAndOpsOnPriotityLevel = getTermsSeqForPriority(terms.tail, ops, terms.head, 0)
+    if(priority > MinPriority) acc else getTermFromSeq(termsAndOpsOnPriotityLevel._1, termsAndOpsOnPriotityLevel._2, priority + 1, acc)
   }
-
-
-
-
 
   def parseTerm(
                  toParse: String,
                  termsAcc: Seq[Term] = Seq.empty[Term],
                  operationsAcc: Seq[BinaryOperation] = Seq.empty[BinaryOperation],
                  numberAcc: String,
-                 parsingNum: Boolean = false,
-                 numberCache: Int,
-                 acc: Term): Term = {
-
-
+                 acc: Term = NumberTerm(0)): Term = {//dont forget 0 is here!!!!
 
     toParse.head match {
-      case c if(c.isDigit) =>
-        val n = numberAcc.appended(c)
-        if(parsingNum == false){
-
-
-        }
-
-        parseTerm(toParse.tail, termsAcc, operationsAcc, n, acc)
+      case c if(c.isDigit) => parseTerm(toParse.tail, termsAcc, operationsAcc, numberAcc.appended(c), acc)
       case SumSign =>
-        parseTerm(toParse.tail, termsAcc, operationsAcc.appended(SumOperation), numberAcc, false, acc)
+        if(numberAcc.length > 0){//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DRY!!!
+          val numToCache = numberAcc.toInt
+          parseTerm(toParse.tail, termsAcc.appended(NumberTerm(numToCache)), operationsAcc.appended(SumOperation), "", acc)//add digit tio cache and operation to remember
+        } else {
+          throw new Exception("Operation symbol is in incorrect position")
+        }
       case SubtractSign => //DRY!!!
-        parseTerm(toParse.tail, termsAcc, operationsAcc.appended(SubtractOperation), numberAcc, false, acc)
+        if(numberAcc.length > 0){//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DRY!!!
+          val numToCache = numberAcc.toInt
+          parseTerm(toParse.tail, termsAcc.appended(NumberTerm(numToCache)), operationsAcc.appended(SubtractOperation), "", acc)//add digit tio cache and operation to remember
+        } else {
+          throw new Exception("Operation symbol is in incorrect position")
+        }
       case MultiplySign =>
-        parseTerm(toParse.tail, termsAcc, operationsAcc.appended(MultiplyOperation), numberAcc, false, acc)
+        if(numberAcc.length > 0){//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DRY!!!
+          val numToCache = numberAcc.toInt
+          parseTerm(toParse.tail, termsAcc.appended(NumberTerm(numToCache)), operationsAcc.appended(MultiplyOperation), "", acc)//add digit tio cache and operation to remember
+        } else {
+          throw new Exception("Operation symbol is in incorrect position")
+        }
       case DevideSign =>
-        parseTerm(toParse.tail, termsAcc, operationsAcc.appended(DevideOperation), numberAcc, false, acc)
+        if(numberAcc.length > 0){//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DRY!!!
+          val numToCache = numberAcc.toInt
+          parseTerm(toParse.tail, termsAcc.appended(NumberTerm(numToCache)), operationsAcc.appended(DevideOperation), "", acc)//add digit tio cache and operation to remember
+        } else {
+          throw new Exception("Operation symbol is in incorrect position")
+        }
       case LParenthesesSign => parseTerm(getTermStringInParentheses(toParse.tail))
     }
 
-
     @scala.annotation.tailrec
-    def getTermStringInParentheses(toParse: String, acc: String = "", openParenthesesNum: Int = 0): String = {
-//      val symbol = toParse.trim.reverse.head
+    def getTermStringInParentheses(toParse: String, acc: String, openParenthesesNum: Int = 0): String = {
       val symbol = toParse.head
-      println("term="+symbol)
-
       symbol match {
         case LParenthesesSign =>
-          println("toParse.tail="+toParse.tail)
           getTermStringInParentheses(toParse.tail, acc.appended(symbol), openParenthesesNum+1)
         case RParenthesesSign =>
-          if(openParenthesesNum - 1 == 0){
-            ((10 - 5) - ((5 +5) * (4 + 4))) * 8
-            acc.appended(RParenthesesSign)
-          } else {
-            getTermStringInParentheses(toParse.tail, acc.appended(symbol), openParenthesesNum - 1)
-          }
-        case _ =>
-          println("toParse.tail="+toParse.tail)
-          getTermStringInParentheses(toParse.tail, acc.appended(symbol), openParenthesesNum)
+          if(openParenthesesNum - 1 == 0)acc.appended(RParenthesesSign) else getTermStringInParentheses(toParse.tail, acc.appended(symbol), openParenthesesNum - 1)
+        case _ => getTermStringInParentheses(toParse.tail, acc.appended(symbol), openParenthesesNum)
       }
     }
 
-    val operation = toParse.reverse.head
 
-    val startingTerm = toParse.head match {
-      case SumSign => throw new Exception("Sum symbol in incorrect position")
-      case SubtractSign => throw new Exception("Subtract symbol in incorrect position")
-      case MultiplySign => throw new Exception("Multiply symbol in incorrect position")
-      case DevideSign => throw new Exception("Devide symbol in incorrect position")
-      case LParenthesesSign => getTermStringInParentheses(toParse)
-      case RParenthesesSign => throw new Exception("right parentheses is in unexpected place")
-      case _ => if(operation.isDigit){"is digit"} else {"not digit"}
-    }
+Option
+
+//    val operation = toParse.reverse.head
 
 //    def getFirstTermFromLeft(s: String) = {
 //      println("getFirstTermFromLeft")
@@ -150,9 +164,24 @@ object PolishCalculator {
   }
 
 
+5 5 7 4 3 4
+* - + / -
+//
+//  term(5,5,*)7434
+//  -+/-
+//
+//  term(5,5,*) term(7) term(4 3 /) term(3,4,/)
+//  - + -
+//
+//
+//
+//
+//  5574349
+//  -*-+/-
 
 
 
+//5 * 5 - 7 + 4 / 3 - 4
 
 // 5 6 + 7 -      4
 // 3 2 * 5 +    11
